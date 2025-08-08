85f40349f4589de3972cf1114dc1417dfe34d063
Nay Chi Cho <naychicho.dev@gmail.com>
2025-08-08 08:11:15 +0630

    ecommerceTest

---
+++ admin/indexSimple.php
@@ -0,0 +1,16 @@
+<?php 
+$batch = 111;
+$name = "IMC";
+$temp = 33.5;
+    echo "<em><b>Hello Batch $batch, welcome to ecommerce module</b></em>";
+
+    function power($num1, $num2)
+    {
+        return pow($num1, $num2);
+    }
+
+    $number1 = 2;
+    $number2 = 6;
+    $result = power($number1, $number2);
+    echo "<br>result is $result";
+?>
